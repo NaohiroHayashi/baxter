7999b4781e91e0fcc8662dfe007b9dcfdbe11a30
Naohiro Hayashi <N.Hayashi1134@gmail.com>
2014-09-21 18:23:00 +0900

    Update pcl.cpp

--- pcl.cpp
+++ pcl.cpp
@@ -28,34 +28,34 @@ using namespace std;
 class Convex_hull{
     private:
     ros::NodeHandle n;
-    ros::Publisher pub, pub2, pub3;
+    ros::Publisher pub1, pub2, pub3;
     ros::Subscriber sub;
-
+    sensor_msgs::PointCloud2 pub_data1, pub_data2, pub_data3;
+    
     public:
     Convex_hull(){
-        pub = n.advertise<sensor_msgs::PointCloud2> ("output", 1);
-        pub2 = n.advertise<sensor_msgs::PointCloud2> ("output2", 1);
-        pub3 = n.advertise<sensor_msgs::PointCloud2> ("output3", 1);
+        pub1 = n.advertise<sensor_msgs::PointCloud2> ("filter", 1);
+        pub2 = n.advertise<sensor_msgs::PointCloud2> ("palane", 1);
+        pub3 = n.advertise<sensor_msgs::PointCloud2> ("convex_hull", 1);
         sub = n.subscribe("camera/depth/points", 1, &Convex_hull::cloud_cb, this);
 
     }
     
     void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& cloud)
     {
-            sensor_msgs::PointCloud2 cloud_box, pub_data, pub_data3;
             pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_projected (new pcl::PointCloud<pcl::PointXYZ>);
             pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZ>);
             pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_hull (new pcl::PointCloud<pcl::PointXYZ>);
             pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_data (new pcl::PointCloud<pcl::PointXYZ>);
-            pcl::ConcaveHull<pcl::PointXYZ> chull;
-            //pcl::PointCloud<pcl::PointXYZ> cloud_data;
+            pcl::ConvexHull<pcl::PointXYZ> chull;
+            
             // Perform the actual filtering
             pcl::VoxelGrid<sensor_msgs::PointCloud2> sor;
             sor.setInputCloud (cloud);
             sor.setLeafSize (0.1, 0.1, 0.1);
-            sor.filter (cloud_box);
-            pcl::fromROSMsg (cloud_box, *cloud_data);
-            pub2.publish (cloud_box);
+            sor.filter (pub_data1);
+            pcl::fromROSMsg (pub_data1, *cloud_data);
+            pub1.publish (pub_data1);
 
             pcl::PassThrough<pcl::PointXYZ> pass;
             pass.setInputCloud (cloud_data);
@@ -65,15 +65,13 @@ class Convex_hull{
             std::cerr << "PointCloud after filtering has: " << cloud_filtered->points.size () << " data points." << std::endl;
             pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
             pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+            
             // Create the segmentation object
             pcl::SACSegmentation<pcl::PointXYZ> seg;
-            // Optional
-            seg.setOptimizeCoefficients (true);
-            // Mandatory
+            seg.setOptimizeCoefficients (true);// Optional
             seg.setModelType (pcl::SACMODEL_PLANE);
             seg.setMethodType (pcl::SAC_RANSAC);
             seg.setDistanceThreshold (0.01);
-
             seg.setInputCloud (cloud_filtered);
             seg.segment (*inliers, *coefficients);
             std::cerr << "PointCloud after segmentation has: " << inliers->indices.size () << " inliers." << std::endl;
@@ -86,16 +84,15 @@ class Convex_hull{
             proj.setModelCoefficients (coefficients);
             proj.filter (*cloud_projected);
             std::cerr << "PointCloud after projection has: " << cloud_projected->points.size () << " data points." << std::endl;
-            pcl::toROSMsg (*cloud_projected, pub_data3);
-            pub3.publish (pub_data3);
+            pcl::toROSMsg (*cloud_projected, pub_data2);
+            pub2.publish (pub_data2);
             
-            // Create a Concave Hull representation of the projected inliers
+            // Create a ConvexHull representation of the projected inliers
             chull.setInputCloud (cloud_projected);
-            chull.setAlpha (0.1);
+            //chull.setAlpha (0.1);
             chull.reconstruct (*cloud_hull);
-            pcl::toROSMsg (*cloud_hull, pub_data);
-            // Publish the data
-            pub.publish (pub_data);
+            pcl::toROSMsg (*cloud_hull, pub_data3);
+            pub3.publish (pub_data3);
     }
 };
 
